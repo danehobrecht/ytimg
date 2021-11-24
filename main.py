691c9b04ed1b18e28771ee8605599895838efd49
tmuq <76x5l22l@anonaddy.me>
2021-11-23 22:42:14 -0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/venv/bin/python3
 
-def main(url):
-	if "https://youtu.be/" in url or "https://www.youtube.com/watch?v=" in url:
-		l = len(url)
-		uuid = url[l - 11:]
-		maxresdefault = "https://img.youtube.com/vi/" + uuid + "/maxresdefault.jpg"
-		print("Video UUID:\t" + uuid + "\nThumbnail URL:\t" + maxresdefault)
-		return maxresdefault
+def main(user_input):
+	if "https://youtu.be/" in user_input or "https://www.youtube.com/watch?v=" in user_input:
+		l = len(user_input)
+		uuid = user_input[l - 11:]
+		url = "https://img.youtube.com/vi/" + uuid + "/maxresdefault.jpg"
+		print("Video UUID:\t" + uuid + "\nThumbnail URL:\t" + url)
+		return url
 	else:
 		return "http://127.0.0.1:5000/"
